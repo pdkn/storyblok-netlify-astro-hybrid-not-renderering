379d0aff1050bc398ce9342dac6c6b95b57ca40b
Paddy Keane <Paddy.Keane@akqa.com>
2024-01-12 11:51:07 -1000

    Add Middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,12 @@
+import type { MiddlewareHandler } from "astro";
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+	const url = new URL(context.request.url)
+	if (url.pathname === '/ssr' || url.pathname.startsWith('/ssr/')) {
+		
+	}
+	const page = await next();
+	// console.log("context.request", context.request);
+	// console.log("page", page);
+	return page
+};
