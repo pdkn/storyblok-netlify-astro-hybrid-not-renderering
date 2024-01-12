d8dd81937a8c522c5408bcfa3728346b80a3f295
Paddy Keane <Paddy.Keane@akqa.com>
2024-01-12 11:55:16 -1000

    Update index.ts

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,7 +3,7 @@ import type { MiddlewareHandler } from "astro";
 export const onRequest: MiddlewareHandler = async (context, next) => {
 	const url = new URL(context.request.url)
 	if (url.pathname === '/ssr' || url.pathname.startsWith('/ssr/')) {
-		
+		// Add Auth here
 	}
 	const page = await next();
 	// console.log("context.request", context.request);
